69f8b2228ce99e06edf3d6ac2419934b4afb0830
Yousef <ysf.alshamali@gmail.com>
2020-07-01 17:50:20 +0300

    I finished the classwork

---
+++ Android_Project8/app/src/main/java/com/example/android_project8/Movie.java
@@ -0,0 +1,9 @@
+package com.example.android_project8;
+
+public class Movie {
+    String title = "The Jungle";
+    String mainActor = "Yousef Saleh";
+    double movierate =  56.3;
+    int pgrate = 7;
+    String genre  = "fiction";
+}
